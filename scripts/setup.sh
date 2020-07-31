c78b3e0648206955434bb545a25bf0fe51f84834
Oliver Benns <oliverbenns@gmail.com>
2020-07-31 10:58:05 +0100

    Add missing nvm install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,6 +15,7 @@ git config --global alias.st status
 git config push.default current
 
 # Install nvm, node and global deps
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
