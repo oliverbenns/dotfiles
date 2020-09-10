3839f57866338e0fc4af8b108d7047e88b627df6
Oliver Benns <oliverbenns@gmail.com>
2020-09-10 23:03:11 +0100

    Copy sublime package list

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -68,6 +68,7 @@ git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 # Some apps have specific config directories, so cp these. Symlinks don't work.
 # Not ideal but this will likely be removed if I transition to vim and standard terminal.
 cp -r $BASE_DIR/../.config/sublime/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+cp -r $BASE_DIR/../.config/sublime/Package\ Control.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 cp -r $BASE_DIR/../.config/iterm2/com.googlecode.iterm2.plist $HOME/Library/Preferences
 
 # See the bash changes
