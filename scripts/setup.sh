da4e15bb7a775d763809c7d72e40183ba46d7818
Oliver Benns <oliver@benns.co>
2025-11-14 12:21:52 +0700

    Update nvm

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,7 +18,7 @@ git config --global push.default current
 git config --global core.excludesfile '~/.gitignore_global'
 
 # Install nvm, node and global deps
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
