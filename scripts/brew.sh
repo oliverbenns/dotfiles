65b6b30f78d47b481578932468520742c3b45c42
Oliver Benns <oliver@benns.co>
2025-11-29 09:52:14 +0700

    Move brew

---
+++ scripts/brew.sh
@@ -0,0 +1,34 @@
+brew install direnv
+brew install gh
+brew install go
+brew install http-server
+brew install killport
+
+brew install --cask brave-browser
+brew install --cask cursor
+brew install --cask chatgpt
+brew install --cask claude
+brew install --cask crossover
+brew install --cask discord
+brew install --cask datagrip
+brew install --cask docker-desktop
+brew install --cask google-chrome
+brew install --cask google-cloud-sdk
+brew install --cask google-drive
+brew install --cask imageoptim
+brew install --cask iterm2
+brew install --cask obs
+brew install --cask postman
+brew install --cask private-internet-access
+brew install --cask qbittorrent
+brew install --cask raycast
+brew install --cask rectangle
+brew install --cask slack
+brew install --cask sourcetree
+brew install --cask spotify
+brew install --cask steam
+brew install --cask telegram
+brew install --cask the-unarchiver
+brew install --cask vlc
+brew install --cask whatsapp
+brew install --cask zoom
