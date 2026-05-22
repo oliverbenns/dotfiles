4930854aa6d97dea419f084cd4d20f7d0603fe19
Oliver Benns <oliver@benns.co>
2026-05-22 10:24:41 +0700

    Various dependency updates

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,28 +1,35 @@
+brew install awscli
+brew install cloudflare-wrangler
 brew install direnv
 brew install fnm
 brew install gh
 brew install go
 brew install http-server
 brew install killport
-brew install libpq
 brew install pnpm
+brew install vercel-cli
 
+brew install --cask android-studio
 brew install --cask brave-browser
-brew install --cask cursor
+brew install --cask bruno
 brew install --cask chatgpt
 brew install --cask claude
+brew install --cask claude-code
+brew install --cask codex
 brew install --cask crossover
-brew install --cask discord
+brew install --cask cursor
 brew install --cask datagrip
+brew install --cask discord
 brew install --cask docker-desktop
+brew install --cask gcloud-cli
 brew install --cask ghostty
 brew install --cask google-chrome
-brew install --cask google-cloud-sdk
 brew install --cask google-drive
 brew install --cask imageoptim
+brew install --cask lm-studio
 brew install --cask obs
+brew install --cask opencode-desktop
 brew install --cask pika
-brew install --cask postman
 brew install --cask private-internet-access
 brew install --cask qbittorrent
 brew install --cask raycast
